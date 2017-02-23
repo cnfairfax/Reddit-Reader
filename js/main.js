1a38b5afb97857527c25a1ad15ba6344e42fe0f9
DESKTOP-540DH91\JDMPC1 <cfairfax2010@gmail.com>
2017-02-22 23:50:12 -0500

    Added background image to anchors and some simple styling. added google fonts Merriweather and Work Sans

--- js/main.js
+++ js/main.js
@@ -5,9 +5,15 @@ $(document).ready(function(){
 		success: function(json) {
 			if(json.data){
 				$.each(json.data.children, function(i, v) {
-					body.append('<a target="_blank" href="' + v.data.url + '">' + v.data.title + '</a><br>');
+                    if(v.data.preview) {
+					body.append('<a target="_blank" style="background-image: url(' + v.data.preview.images[0].source.url + ');" href="' + v.data.url + '"><h2>' + v.data.title + '</h2></a>');
+                    } else {
+                        body.append('<a target="_blank" href="' + v.data.url + '"><h2>' + v.data.title + '</h2></a>');
+                    }
 				});
+                console.log(json.data.children);
 			}
+            
 		}
 	})
 });
