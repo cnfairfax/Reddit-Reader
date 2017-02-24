ef5b841ab931672ce3c4b6ea2d4a7d552d96e2e5
DESKTOP-540DH91\JDMPC1 <cfairfax2010@gmail.com>
2017-02-24 10:45:04 -0500

    Nav at bottom Style changes

--- js/main.js
+++ js/main.js
@@ -30,17 +30,22 @@ $(document).ready(function(){
                     const info = $('.info');
                     const infoCard = cardId + ' ' + info;
                     if(v.data.preview) {
+                        /*if(v.data.spoiler){
+                            content.append('<a target="_blank" href="" class="post-card" id="card' + i + '"><div class="info"><h2>SPOILER</h2><div>');
+                            $(cardId).addClass('background' + t);
+                            $(cardId).click(function() {
+                                $('.info').append('<h2>' + v.data.title + '</h2>');
+                                $('.post-card').attr('href', v.data.url);
+                                $('.post-card').attr('style', 'background: url(' + v.data.preview.images[0].source.url + ');');
+                            });
+                        }*/
 					   content.append('<a target="_blank" style="background-image: url(' + v.data.preview.images[0].source.url + ');" href="' + v.data.url + '" class="post-card" id="card' + i + '"><div class="info"><h2>' + v.data.title + '</h2><div>');
                     } else {
                         content.append('<a target="_blank" href="' + v.data.url + '" class="post-card" id="' + card + '"><div class="info"><h2>' + v.data.title + '</h2></div></a>');
                         $(cardId).addClass('background' + t);
                     }
-            
-                    if(v.data.over_18 || v.data.spoiler) {
-                        $(cardId).addClass('blur');
-                    }
                     
-                    $(cardId + ' .info').append('<div class="post-data"><p><a target="_blank" href="https://www.reddit.com' + v.data.permalink + '">Comments</a></p><p>Posted By: <a target="_blank" href="https://www.reddit.com/u/' + v.data.author + '">' + v.data.author + '</a></p><p class="score">' + v.data.score + '</p></div>');
+                    $(cardId + ' .info').append('<div class="post-data"><p><a target="_blank" href="https://www.reddit.com' + v.data.permalink + '">' + v.data.num_comments + ' Comments</a></p><p>Posted By: <a target="_blank" href="https://www.reddit.com/u/' + v.data.author + '">' + v.data.author + '</a></p><p class="score">' + v.data.score + '</p></div>');
                     
 				});
                 console.log(json.data.children);
