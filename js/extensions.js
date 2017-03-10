0f4a15bbdcbb10b01af0d2e1d48f852bd44c1ed6
Collin Hulon <collin@jdmfx.com>
2017-03-10 18:55:19 -0500

    Begin templating, method externalization through extentions (underscore and jQuery)

---
+++ js/extensions.js
@@ -0,0 +1,28 @@
+_.mixin({
+	findPrimeFactor: function(n) {
+		var i=2;
+		while (i<=n){
+			if (n%i == 0){
+				n/=i;    
+			}else{
+				i++;
+			}
+		}
+		return i;
+	},
+	nav: function(sub) {
+		return {
+			name: '/r ' + sub,
+			url: 'https://www.reddit.com/r/' + sub + '.json'
+		};
+	}
+});
+
+$.fn.extend({    //create default methods that you want to define
+    do: function(callback){
+		return (this.length ? callback : $.noop).call(this, this) || this;
+	},
+    fire: function(event){
+		return this.trigger(event);
+	}
+});
