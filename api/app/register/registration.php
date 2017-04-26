b6206f7613009ba22d3c32f8f907dcd2af58e0d6
Chris Fairfax <cfairfax2010@gmail.com>
2017-04-26 17:38:07 -0400

    registering a new user worked once...not sure why, though

--- api/app/register/registration.php
+++ api/app/register/registration.php
@@ -1,18 +1,13 @@
 <?
     return new class {
         function run($request) {
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $database = "reddit_data";
+            $DB_HOST = 'localhost';
+            $DB_USER = 'root';
+            $DB_PASS = '';
+            $DB_NAME = 'reddit_data';
 
             // Create connection
-            $conn = new mysqli($servername, $username, $password, $database);
-
-            // Check connection
-            if (mysqli_connect_errno()){
-                return json_encode("Failed to connect to MySQL: " . mysqli_connect_error());
-            }
+            $conn = @mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
             $method = $_SERVER['REQUEST_METHOD'];
             
@@ -21,22 +16,33 @@
                 // If post, check for inputs
                 if((isset($request->email) && !empty($request->email)) && (isset($request->password) && !empty($request->password))) {
                     // Assign inputs to variables
-                    $new_user = $request->email;
-                    $new_pw = $request->password;
+                    $new_user = trim($request->email);
+                    $new_pw = trim($request->password);
 
                     // Check to make sure username isn't already in use
-                    $check_query = "SELECT * FROM users WHERE username=".$new_user;
-                    $clean_check_query = mysqli_escape_string($check_query);
-                    print_r($conn);
-                    //$get_check = mysqli_query($conn, $clean_check_query);
-                    //$check_rows = mysqli_fetch_assoc($get_check);
-                    
-                    //if($get_check === 'FALSE') {
-                       // return json_encode("Currently no user $new_user in database")
-                   // }
-                   // else {
-                        //return json_encode("User, $new_user - $new_pw already exists");
-                   // }
+                    $check_query = "SELECT username, password FROM users WHERE username='" . $new_user . "'";
+                    $get_check = @mysqli_query($conn, $check_query);
+                    $check_rows = @mysqli_fetch_assoc($get_check);
+
+                    // Check to make sure $check_rows == false
+                    if(!$check_rows) {
+                        $new_pw_hash = password_hash($new_pw, PASSWORD_DEFAULT);
+                        $insert_user_query = "INSERT INTO users (username, password) VALUES (?, ?)";
+                        $insert_user = mysqli_prepare($conn, $insert_user_query);
+
+                        mysqli_stmt_bind_param($insert_user, "ss", $new_user, $new_pw_hash);
+
+                        mysqli_stmt_execute($insert_user);
+
+                        $affected_rows = @mysqli_stmt_affected_rows($insert_user);
+                        print_r($affected_rows);
+                        //if($affected_rows == 1) {
+                            return json_encode("User, $new_user added!")
+                        //}
+                    }
+                    else {
+                        return json_encode("User, $new_user already exists");
+                    }
                 }
                 else {
                     return json_encode('Please enter an email and password');
@@ -45,8 +51,6 @@
             else {
                 return json_encode("Could not complete request.");
             }
-
-            mysqli_close();
         }
     }
 ?>
